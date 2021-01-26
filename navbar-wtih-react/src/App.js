d36ea442b03aea8f9bd69d4149a0550259084e5b
max-vac <maxvaclavik@gmail.com>
2021-01-25 22:08:35 -0800

    Fixed props and information for the navbar

--- navbar-wtih-react/src/App.js
+++ navbar-wtih-react/src/App.js
@@ -1,29 +1,42 @@
-import { amber } from '@material-ui/core/colors';
-import logo from './logo.svg';
+// import { amber } from '@material-ui/core/colors';
 import Sidebar from './Sidebar';
+import HomeIcon from '@material-ui/icons/Home';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import StoreIcon from '@material-ui/icons/Store';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import SettingsIcon from '@material-ui/icons/Settings';
+import PaymentIcon from '@material-ui/icons/Payment';
+import InfoIcon from '@material-ui/icons/Info';
+import AddIcon from '@material-ui/icons/Add';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const listItems = [
-  { name: 'home', label: 'Home' },
+  { name: 'home', label: 'Home', Icon: HomeIcon },
   { name: 'menu',
     label: 'Menu',
+    icon: MenuIcon,
     listItems: [
-      { name: 'orders', label: 'Orders'},
-      { name: 'recent purchases', label: 'Recent Purchases'},
+      { name: 'orders', label: 'Orders', icon: StoreIcon},
+      { name: 'recent purchases', label: 'Recent Purchases', icon: AddIcon},
       { name: 'cart',
         label: 'Cart',
+        icon: AddShoppingCartIcon,
         listItems: [
-          { name: 'recently added', label: 'Recently Added' },
-          { name: 'go to cart', label: 'Go To Cart' }
+          { name: 'recently added', label: 'Recently Added', icon: AddIcon },
+          { name: 'go to cart', label: 'Go To Cart', icon: ShoppingCartIcon }
         ]
     }
     ]
   },
+  "divider",
   { name: 'account',
     label: 'Account',
+    icon: AccountBoxIcon,
     listItems: [
-      { name: 'information', label: 'Information' },
-      { name: 'payments', label: 'Payments' },
-      { name: 'settings', label: 'Settings' }
+      { name: 'information', label: 'Information', icon: InfoIcon },
+      { name: 'payments', label: 'Payments', icon: PaymentIcon },
+      { name: 'settings', label: 'Settings', icon: SettingsIcon }
     ]
   }
 ];
@@ -31,7 +44,7 @@ const listItems = [
 
 const App = () => {
   return (
-    <Sidebar listItems={ listItems } />
+    <Sidebar menuItems={ listItems } />
   )
 }
 
--- navbar-wtih-react/src/Sidebar.js
+++ navbar-wtih-react/src/Sidebar.js
@@ -2,16 +2,19 @@ import React from 'react';
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import Divider from "@material-ui/core/Divider";
 
 const style = {
   maxWidth: "200px",
   border: "1px solid rgba(0, 0, 0, 0.1)",
 };
 
-function SidebarItems({ label, listItems, depthStep = 10, depth = 0, ...rest}) {
+function SidebarItems({ depthStep = 20, depth = 0, expanded, menuItems, ...rest }) {
+  const [collapsed, setCollapsed] = React.useState(true);
+  const { label, listItems, Icon } = menuItems;
   return (
     <div>
-      <ListItem button dense {...rest}>
+      <ListItem button dense >
         <ListItemText style={{ paddingLeft: depth * depthStep }}>
           <span>{label}</span>
         </ListItemText>
@@ -23,25 +26,33 @@ function SidebarItems({ label, listItems, depthStep = 10, depth = 0, ...rest}) {
               key={ items.name }
               depth={ depth + 1}
               depthStep={ depthStep }
-              {...items}
-            />
+              menuItems={ items }
+              />
           ))}
         </List>
       ) : null }
     </div>
   );
-}
-function Sidebar({ listItems, depthStep, depth }) {
+};
+
+function Sidebar({ menuItems, depthStep, depth }) {
+
   return (
     <div style={ style }>
       <List disablePadding dense>
-        {listItems.map(( item, index ) => (
-          <SidebarItems
-            key={ `${item}${index}`}
-            depthStep={ depthStep }
-            depth={ depth }
-            { ...item }
-          />
+        {menuItems.map(( items, index ) => (
+          <React.Fragment key={ `${items.name}${index}` }>
+          {items === "divider" ? (
+            <Divider style={{ margin: "6px 0" }} />
+          ) : (
+            <SidebarItems
+              key={ `${items.name}${index}`}
+              depthStep={ depthStep }
+              depth={ depth }
+              menuItems={ items }
+            />
+          )}
+          </React.Fragment>
         ))}
       </List>
     </div>
@@ -49,3 +60,4 @@ function Sidebar({ listItems, depthStep, depth }) {
 }
 
 export default Sidebar;
+
